17f26dd4b9b2257ced169deb24329f855ea8105f
Your Name <you@example.com>
2024-10-25 12:51:41 +0530

    emailjs needs to be fixed

---
+++ src/constants/index.js
@@ -0,0 +1,212 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  tesla,
+  shopify,
+  spotify,
+  sathi,
+  tripguide,
+  threejs,
+} from "../assets";
+import codetikki from "../assets/company/codetikki.jpeg";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Web Developer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Backend Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "MERN Stack Developer",
+    company_name: "CodeTikki",
+    icon: codetikki,
+    iconBg: "#383E56",
+    date: "January 2023 - March 2023",
+    points: [
+      "Developing and maintaining web applications using React.js,Node.js  and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Lyft Back-End Engineering Virtual Experience Program held by Forage A program that consists of 4 chapter namely software architecture refactoring unit-testing and test driven-development.",
+    name: "Lyft",
+    designation: "SDE intern",
+    company: "Forage",
+    link:"https://forage-uploads-prod.s3.amazonaws.com/completion-certificates/Lyft/xSw9echtixLAoPdsH_Lyft_Zyj746CAXJ5BbCdCK_1700289748124_completion_certificate.pdf",
+    image:
+      "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABCUlEQVR4AWJwL/ABtFvGNhCDMADMBBmBUbLAd6l/HTZgBWag+4oJKJmCDXhHIhKyMI9FENKL4honOBdjgKksgSWwxe0jAZthepNCjhPwQMxwwKskoNGLofPjOB9GDxNI1YwNqMcFYNwORAaCFHj47w0Rl1WBwgCf4hY/o+LAnp4JqhIcAZfihhDwpfjNFAFAXcwTSMwWCBUBP1PA9a0C/hSE0irhCEQiSU1AXODK4R5QnKZhCFi0DA9K4E0k1D0C+OSjqrARg1WKswSoE68mgffykDccqkQA1I8mdMDZcG4ct0jLAXNk+7miltmIKxkuK4UcKeBa5n9mBcQwAdSYeDOSf3MtXwJL4AvaqNspo0IrQAAAAABJRU5ErkJggg==",
+  },
+];
+
+const projects = [
+  {
+    name: "Music Player",
+    description:
+      "Web-based platform that allows users to search, upload and play audio files from various providers, providing a convenient and efficient solution for online music needs.",
+    tags: [
+      {
+        name: "NextJS",
+        color: "black-text-gradient",
+      },
+      {
+        name: "Supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: spotify,
+    source_code_link: "https://github.com/mrayushmehrotra/spotify-clone",
+  },
+  {
+    name: "Sathi | AI ",
+    description:
+      "Web application that enables users get better SEO friendly title, description and trending hashtags for their content, which can help them get more views on social media platform.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: sathi,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Trip Guide",
+    description:
+      "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: tripguide,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
